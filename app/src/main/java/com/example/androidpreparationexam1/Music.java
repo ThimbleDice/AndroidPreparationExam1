7073527b5d3150bdf247cc581e8f96f4ce64e049
Thimmbledice <thimbledice@gmail.com>
2019-04-06 19:18:38 -0400

    Beginning of music class

---
+++ app/src/main/java/com/example/androidpreparationexam1/Music.java
@@ -0,0 +1,31 @@
+package com.example.androidpreparationexam1;
+
+import android.media.MediaPlayer;
+
+public class Music {
+    MediaPlayer mediaPlayer;
+
+    public Music(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public void setMediaPlayer(MediaPlayer mediaPlayer) {
+        this.mediaPlayer = mediaPlayer;
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    public void start(){
+
+    }
+
+    public void stop(){
+
+    }
+
+    public void goTo(){
+
+    }
+}
